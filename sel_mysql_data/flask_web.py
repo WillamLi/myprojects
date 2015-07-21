8d189aad26bc160c648b5fefe4c03883dc47bd7f
WillamLi <ranran840@qq.com>
2015-07-21 16:31:06 +0800

    饼图示例，注意echarts和highcharts

---
+++ sel_mysql_data/flask_web.py
@@ -0,0 +1,28 @@
+#coding=utf8
+from flask import Flask,request,render_template,session,url_for,redirect
+import MySQLdb as mysql
+import json
+
+app=Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/chart_data')
+def chart_data():
+    m=[]
+    m.append(['PE','CMF'])
+    m.append({'value':200,'name':'PE'})
+    m.append({'value':30,'name':'cmf'})
+    print m
+    o={}
+    o['DBNAME']=m[0]
+    o['data']=m[1:]
+    print json.dumps(o)
+    return json.dumps(o)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=33516,debug=True)
