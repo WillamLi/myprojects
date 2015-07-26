d6d6f6551e1e05d6ac5ca1a8296cb26ea36b8206
WillamLi <ranran840@qq.com>
2015-07-26 23:26:58 +0800

    ok_threading

--- sel_mysql_data/flask_web.py
+++ sel_mysql_data/flask_web.py
@@ -20,8 +20,26 @@ def chart_data():
     o={}
     o['DBNAME']=m[0]
     o['data']=m[1:]
+    o['t']=123
     print json.dumps(o)
     return json.dumps(o)
+
+
+@app.route('/chart_data2')
+def chart_data2():
+    m=[]
+    m.append(['PE','CMF'])
+    m.append({'value':200,'name':'PE'})
+    m.append({'value':30,'name':'cmf'})
+    print m
+    o={}
+    o['DBNAME']=m[0]
+    o['data']=m[1:]
+    o['t']=456
+    print json.dumps(o)
+    return json.dumps(o)
+
+
 
 
 if __name__ == '__main__':
--- sel_mysql_data/test.py
+++ sel_mysql_data/test.py
@@ -2,13 +2,29 @@
 import threading
 import urllib2
 import json
+from time import ctime,sleep
 
 def a(ip):
     au=urllib2.urlopen(ip).read()
     print au
     au_j=json.loads(au)    
     print au_j
-    print au_j["data"]
+    print au_j["t"]
 
+iplist=["http://180.153.191.128:33516/chart_data","http://180.153.191.128:33516/chart_data"]
 
-a("http://180.153.191.128:33516/chart_data")
+threads=[]
+for i in iplist:
+    print ctime()
+    t=threading.Thread(target=a,args=(i,))
+    threads.append(t)
+    print threads
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
+print "t:",ctime()
+#a("http://180.153.191.128:33516/chart_data")
